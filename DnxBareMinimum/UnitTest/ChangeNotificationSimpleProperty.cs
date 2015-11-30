0dfa9cf9124a0c8fb2d27217e958c99f74cbc82b
aadennis <dennis.circ@gmail.com>
2015-11-30 22:49:15 +0000

    Tired - off now
    Tired - off now (but all 12 tests in the earlier commit were passing)

---
+++ DnxBareMinimum/UnitTest/ChangeNotificationSimpleProperty.cs
@@ -0,0 +1,25 @@
+using Model;
+using Xunit;
+
+namespace UnitTests {
+	
+	public class ChangeNotificationSimpleProperty {
+		
+		Model.Address _address;
+	
+		public ChangeNotificationSimpleProperty() {
+			TestInitialize();
+		}
+		
+		[Fact]
+		public void StuffDoesStuff() {
+			Assert.False(1 == 1);
+		}
+
+
+		private void TestInitialize() {
+				_address = new Address{Line1 = "60 Acacia Avenue", Line2 = "Tottenham"};
+			
+		}
+	}
+}
--- DnxBareMinimum/ViewModel/Observable.cs
+++ DnxBareMinimum/ViewModel/Observable.cs
@@ -1,8 +1,20 @@
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace ViewModel {
 	
 	public class Observable : INotifyPropertyChanged {
 		public event PropertyChangedEventHandler PropertyChanged;
+		
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) {
+			var handler = PropertyChanged;
+			if (handler != null) {
+				handler(this, new PropertyChangedEventArgs(propertyName));
+			}
+			
+			
+		}
+		
+		
 	}
 }
